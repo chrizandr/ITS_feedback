735295812de5a2e695647b410c4a9e46d88479bb
chrizandr <chris.g14@iiits.in>
2017-02-19 18:20:40 +0530

    Starting

--- feedback_portal/feedback_portal/settings.py
+++ feedback_portal/feedback_portal/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'main',
 ]
+
+GRAPH_MODELS = {
+  'all_applications': True,
+  'group_models': True,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
